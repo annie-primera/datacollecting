b03e5123e50a0c65aed65fe19d862c8bc0a78bf0
ahibert <ahibertjr@gmail.com>
2018-09-18 16:32:23 +0100

    login and registration work

---
+++ dbhelper.py
@@ -0,0 +1,15 @@
+import pymongo
+
+DATABASE = "datacollection"
+
+
+class DBHelper:
+    def __init__(self):
+        client = pymongo.MongoClient()
+        self.db = client[DATABASE]
+
+    def get_user(self, email):
+        return self.db.users.find_one({"email": email})
+
+    def add_user(self, email, salt, hashed):
+        self.db.users.insert({"email": email, "salt": salt, "hashed": hashed})
